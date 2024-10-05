9ecfb7b2d2e088932dd73041840998fe734af193
Jean-Elie <jeaneliekubembula@gmail.com>
2024-10-05 14:30:21 +0100

    Ajout des permission parent, élève et enseignant

--- backend/permissions/permission_app.py
+++ backend/permissions/permission_app.py
@@ -21,3 +21,24 @@ class IsAccountant(BasePermission):
     """
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.role.name == 'Comptable'
+    
+class IsTeacher(BasePermission):
+    """
+    Permission pour les utilisateurs ayant le rôle de enseignant.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role.name == 'Enseignant'
+
+class IsPupil(BasePermission):
+    """
+    Permission pour les utilisateurs ayant le rôle de élève.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role.name == 'Élève'
+
+class IsParent(BasePermission):
+    """
+    Permission pour les utilisateurs ayant le rôle de parent.
+    """
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role.name == "Parent d'élève"
