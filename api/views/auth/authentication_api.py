3f600724a99fba3345380959c034debac3aac9f0
Jean-Elie <jeaneliekubembula@gmail.com>
2025-01-18 12:38:56 +0100

    maj

--- api/views/auth/authentication_api.py
+++ api/views/auth/authentication_api.py
@@ -14,8 +14,8 @@ def get_tokens_for_user(user):
     refresh = RefreshToken.for_user(user)
 
     return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
+        'refresh_token': str(refresh),
+        'access_token': str(refresh.access_token),
     }
 
 
